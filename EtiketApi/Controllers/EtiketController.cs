0b349683009dc502ade65187f4b444168e96a3d3
Fikret-Cin <cinfikret@gmail.com>
2020-09-18 09:41:39 +0300

    First commit -Inıt

---
+++ EtiketApi/Controllers/EtiketController.cs
@@ -0,0 +1,73 @@
+﻿using iTextSharp.text.pdf;
+using System.IO;
+using System.Web.Http;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using iTextSharp.text.pdf.qrcode;
+
+namespace EtiketApi.Controllers
+{
+    public class EtiketController : ApiController
+    {
+        public HttpResponseMessage Post([FromBody] YaziciData yaziciData)
+        {
+            try
+            {
+                var str = JsonConvert.SerializeObject(yaziciData);
+                string config = @"Server=OOZBEK;Database=EtiketApi;Trusted_Connection = True;";
+                SqlConnection con = new SqlConnection(config);
+                string query = @"INSERT INTO Etiket (Etiket) VALUES ('" + str + "')";
+                SqlCommand cmd = new SqlCommand(query, con);
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            catch (System.Exception)
+            {
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            }
+
+            //Etiket Template Yolu
+            string uygulamaDizini = System.AppDomain.CurrentDomain.BaseDirectory;
+            FileInfo etiketTipi = new FileInfo(@uygulamaDizini + "Templates\\" + yaziciData.Etiket + ".pdf");
+
+            //Template Doldurma            
+            FileInfo temp = new FileInfo(@uygulamaDizini + "Templates\\temp.pdf");
+            PdfReader pdfReader = new PdfReader(etiketTipi.FullName);
+            FileStream stream = new FileStream(temp.FullName,FileMode.Create);
+            PdfStamper pdfStamper = new PdfStamper(pdfReader, stream);
+            AcroFields pdfFormFields = pdfStamper.AcroFields;
+            
+            foreach (var item in yaziciData.EtiketData)
+            {
+                if (item.Key != null && item.Key != "qrCode")
+                {
+                    pdfFormFields.SetField(item.Key.ToString(), item.Value.ToString());
+                }
+                else if (item.Key != null && item.Key == "qrCode" )
+                {
+                    BarcodeQRCode testqr = new BarcodeQRCode(item.Value, 50, 50, null); ;
+                    iTextSharp.text.Image img = testqr.GetImage();
+                    img.SetAbsolutePosition(10,20);
+                    pdfStamper.GetOverContent(1).AddImage (img);
+                }
+            }
+            pdfStamper.FormFlattening = false;
+            pdfStamper.Close();
+
+            //Yazdırma İşlemi
+            Spire.Pdf.PdfDocument yazdirilacakDosya = new Spire.Pdf.PdfDocument();
+            yazdirilacakDosya.LoadFromFile(temp.FullName);
+            yazdirilacakDosya.PrintSettings.PrinterName = yaziciData.YaziciAdi;
+            yazdirilacakDosya.PrintSettings.Copies = (short)yaziciData.EtiketAdedi;
+            yazdirilacakDosya.Print();
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ EtiketApi/YaziciData.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace EtiketApi
+{
+    public class YaziciData
+    {
+        [JsonProperty("yaziciAdi")]
+        public string YaziciAdi { get; set; }
+        [JsonProperty("etiketAdedi")]
+        public int EtiketAdedi { get; set; }
+        [JsonProperty("etiket")]
+        public string Etiket { get; set; }
+
+        [JsonProperty("kaynak")]
+        public string Kaynak { get; set; }
+
+        [JsonProperty("etiketData")]
+        public List<EtiketData> EtiketData { get; set; }
+    }
+    public class EtiketData
+    {
+        [JsonProperty("key")]
+        public string Key { get; set; }
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+
+
+
+}
